76ef11358bdf5cff61d1cf4781ec8cac24760052
Aryan Pandya <100082113+Aryan-136@users.noreply.github.com>
2025-05-10 00:57:48 +0530

    Update update-cleanup-reboot.sh

--- update-cleanup-reboot.sh
+++ update-cleanup-reboot.sh
@@ -1,13 +1,4 @@
 sudo apt update && sudo apt upgrade -y
-
 sudo apt full-upgrade -y
-
-sudo apt-get update && sudo apt-get upgrade -y
-
-sudo apt-get dist-upgrade -y
-
 sudo apt autoremove -y
 sudo apt autoclean
-
-sudo apt-get autoremove -y
-sudo apt-get autoclean
